395896e4956f0d8b3ce38ab9f9da44686f0bb167
mandeepb29 <mandeepb29@gmail.com>
2020-03-10 00:21:22 +0530

    update

--- src/app/all-videos/all-videos.module.ts
+++ src/app/all-videos/all-videos.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllVideosComponent } from './all-videos.component';
+import { RouterModule,Route } from '@angular/router';
 
-
+const routes: Route[]=[
+    {
+    path: '',
+    component: AllVideosComponent
+    }
+  ]
 
 @NgModule({
   declarations: [AllVideosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AllVideosModule { }
